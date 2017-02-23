850b8ed3af73a525474ed93a9f9a639286573304
Inari Listenmaa <inari.listenmaa@gmail.com>
2017-02-23 15:48:36 +0100

    change command line parameters: if no argument, it does everything

--- app/Main.hs
+++ app/Main.hs
@@ -13,19 +13,19 @@ main = do
   case args of
     (rlfile:rest) 
       -> case rest of
-           ("all":_) -> do compact rlfile
-                           repetitiveDefs rlfile
-                           sortByTarget rlfile
            ("compact-strings":_) -> compact rlfile
            ("find-repetition":_) -> repetitiveDefs rlfile
            ("sort-by-target":_) -> sortByTarget rlfile
-           _ -> usage
+           _ -> do compact rlfile
+                   repetitiveDefs rlfile
+                   sortByTarget rlfile
     _ -> usage
 
 usage :: IO ()
 usage = 
   do putStr "usage: stack exec cg-clean <file>.rlx " 
-     putStrLn " < all | compact-strings | find-repetition | sort-by-target >"
+     putStrLn " [ compact-strings | find-repetition | sort-by-target ]"
+     putStrLn "(If no option given after rule file, it does everything.)"
      putStr "Output will be generated in <file>.compact.rlx, " 
      putStrLn " <file>.repetitive.txt, <file>.sorted.txt"
     
