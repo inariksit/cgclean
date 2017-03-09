c38777eb9f1c79be09ead1303d7feca52185ea76
Inari Listenmaa <inari.listenmaa@gmail.com>
2017-03-09 16:27:12 +0100

    add sections to the sorted rule file

--- app/Main.hs
+++ app/Main.hs
@@ -13,8 +13,16 @@ main = do
            ("compact-strings":_) -> compact rlfile
            ("find-repetition":_) -> repetitiveDefs rlfile
            ("sort-by-target":_) -> sortByTarget rlfile
-           _ -> do compact rlfile
+           _ -> do putStrLn "Changing wordforms into case-insensitive regular expressions."
+                   putStrLn "Then, remove redundant rules:"
+                   putStrLn "e.g.  `REMOVE \"<x>\" if y'   and   `REMOVE \"<X>\" IF y'"
+                   putStrLn "will be merged into one rule.\n"
+                   compact rlfile
+
+                   putStrLn "Finding repetitive set definitions"
                    repetitiveDefs rlfile
+
+                   putStrLn "Grouping rules by targets\n"
                    sortByTarget rlfile
     _ -> usage
 
@@ -25,4 +33,4 @@ usage =
      putStrLn "(If no option given after rule file, it does everything.)"
      putStr "Output will be generated in <file>.compact.rlx, " 
      putStrLn " <file>.repetitive.txt, <file>.sorted.rlx."
-    
+
--- src/Clean.hs
+++ src/Clean.hs
@@ -8,17 +8,17 @@ module Clean
 import CGHS
 import CGHS.Compact
 
+import CGSAT
+import Analyse
+
+import SAT ( newSolver, deleteSolver )
+
 import System.FilePath ( takeBaseName )
 
 -------------------------------------------------------------------------------
 
 compact :: FilePath -> IO ()
 compact rlfile = do
-  putStrLn "Changing wordforms into case-insensitive regular expressions."
-  putStrLn "Then, remove redundant rules:"
-  putStrLn "e.g.  `REMOVE \"<x>\" if y'   and   `REMOVE \"<X>\" IF y'"
-  putStrLn "will be merged into one rule."
-  putStrLn ""
   compactGrammar <- printGrammar True `fmap` readFile rlfile
   let compactName = takeBaseName rlfile ++ ".compact.rlx"
   writeFile compactName compactGrammar
@@ -27,8 +27,6 @@ compact rlfile = do
 
 repetitiveDefs :: FilePath -> IO ()
 repetitiveDefs rlfile = do
-  putStrLn "Finding repetitive set definitions"
-  putStrLn ""
   let compact = False
   (defs,rules) <- parse compact `fmap` readFile rlfile
 
@@ -79,15 +77,20 @@ breakInSets _ x = show x
 -- as soon as I get it to work properly for Basque.
 sortByTarget :: FilePath -> IO ()
 sortByTarget rlfile = do
-  putStrLn "Grouping rules by targets"
-  putStrLn ""
   grammar <- readFile rlfile
-
-  let compact = True
-
+  let compact = False
   let (defs,rules) = parse compact grammar
   let groupedRls = map sortByContext $ groupRules $ concat rules
-  
+
+  s <- newSolver
+  (env,_) <- envRules ("eus",[]) s -- TODO: parametrise for any other language
+  conflicts <- mapM (satCheck env) (take 2 groupedRls)
+
+  putStrLn "Found the following conflicts:"
+  mapM_ (mapM_ print) conflicts
+
+  deleteSolver s
+
   let sortedFile = takeBaseName rlfile ++ ".sorted.rlx"
 
   writeFile sortedFile $ printDefs compact grammar
@@ -98,13 +101,30 @@ sortByTarget rlfile = do
 
   showGroup rules = 
     let trg = show $ target (head rules)
-     in ("\n# Rules that target " ++ trg ++ "\n"):
+     in ("\nSECTION\n# Rules that target " ++ trg ++ "\n"):
         map show rules
 
 -- Maybe add a proper printDefs function in CGHS.Parse
 printDefs :: Bool -> String -> String
 printDefs b = unlines . filter isSet . lines . printGrammar b
  where 
+  isSet ('D':'E':'L':_)     = True
   isSet ('S':'E':'T':_)     = True
   isSet ('L':'I':'S':'T':_) = True
   isSet _ = False 
+
+satCheck :: Env -> [Rule] -> IO [(Rule,Conflict)]
+satCheck env rules = do
+  let largestWidth = maximum $ map (fst . width) rules
+  config <- evalRWSE env (mkConfig largestWidth)
+  (maybeConflicts,_,log_) <- rwse env config $ testRules True rules
+
+  mapM_ putStrLn log_
+  case maybeConflicts of
+    Left err -> do print err
+                   return []
+    Right cs -> return (onlyConflicts rules cs)
+
+onlyConflicts :: [Rule] -> [Conflict] -> [(Rule,Conflict)]
+onlyConflicts rules confs = filter (isConflict.snd) $ zip rules confs
+
